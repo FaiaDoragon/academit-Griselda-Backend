d7c0d86f5138cf3a92b0ed62671e2fdeae54e3f5
Francisco Javier Jimenez Cohen <fjimenez-ext@ultratech-inc.com>
2024-06-22 13:41:38 -0300

    add Cursos

--- src/cursos/cursos.service.ts
+++ src/cursos/cursos.service.ts
@@ -3,6 +3,8 @@ import {
   InternalServerErrorException,
   NotFoundException,
   Logger,
+  BadRequestException,
+  ConflictException,
 } from '@nestjs/common';
 import { CreateCursoDto } from './dto/create-curso.dto';
 import { UpdateCursoDto } from './dto/update-curso.dto';
@@ -11,6 +13,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { FindOptionsWhere, Repository } from 'typeorm';
 import { promises as fs } from 'fs';
 import * as path from 'path';
+import { QueryFailedError } from 'typeorm';
 
 @Injectable()
 export class CursosService {
@@ -23,10 +26,10 @@ export class CursosService {
 
   async create(createCursoDto: CreateCursoDto, file: any): Promise<Curso> {
     this.logger.log(
-      `Servicio: CursosService, Método: create, Args: ${JSON.stringify({ createCursoDto, file })}`,
+      `Método: create, Args: ${JSON.stringify({ createCursoDto, file })}`,
     );
 
-    let createCursoData = file
+    const createCursoData = file
       ? { ...createCursoDto, video: file.path }
       : createCursoDto;
 
@@ -35,22 +38,25 @@ export class CursosService {
       await this.cursoRepository.save(curso);
       const cursoToFind = await this.findOne(curso.id);
       if (!cursoToFind) {
-        throw new NotFoundException({
-          message: 'Error al crear el curso.',
-          error: 'Bad Request',
-          statusCode: 400,
-        });
+        throw new BadRequestException('Error al crear el curso.');
       }
       return curso;
     } catch (error) {
       this.logger.error(
-        `Error en Servicio: CursosService, Método: create, Args: ${JSON.stringify({ createCursoDto, file })}, Error: ${error.message}`,
+        `Método: create, Args: ${JSON.stringify({ createCursoDto, file })}, Error: ${error.message}`,
       );
-      throw new InternalServerErrorException({
-        message: error.message,
-        error: error.response?.error,
-        statusCode: error.status,
-      });
+      if (
+        error instanceof QueryFailedError &&
+        error.message.includes('duplicate key value')
+      ) {
+        throw new ConflictException('Ya existe un curso con los mismos datos.');
+      }
+      if (error instanceof BadRequestException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        'Error interno al crear el curso.',
+      );
     }
   }
 
@@ -62,63 +68,66 @@ export class CursosService {
     data: Curso[];
     pagination: { totalItems: number; pageCount: number; currentPage: number };
   }> {
-    this.logger.log('Servicio: CursosService, Método: findAll');
-
-    const { id, title, description, video } = searchParams;
-
-    const dataFilter = { id, title, description, video };
+    this.logger.log('Método: findAll');
 
     const where: FindOptionsWhere<Curso> = {};
 
-    if (dataFilter) {
+    if (searchParams) {
       Object.keys(searchParams).forEach((key) => {
-        if (dataFilter[key] && !['page', 'limit'].includes(key)) {
-          where[key] = dataFilter[key];
+        if (searchParams[key] && !['page', 'limit'].includes(key)) {
+          where[key] = searchParams[key];
         }
       });
     }
 
-    const [cursos, total] = await this.cursoRepository.findAndCount({
-      where,
-      skip: (+page - 1) * limit,
-      take: limit,
-    });
+    try {
+      const [cursos, total] = await this.cursoRepository.findAndCount({
+        where,
+        skip: (+page - 1) * limit,
+        take: limit,
+      });
 
-    if (cursos.length === 0) {
-      throw new NotFoundException('No se encontraron encabezados.');
+      if (cursos.length === 0) {
+        throw new NotFoundException('No se encontraron cursos.');
+      }
+
+      const pagination = {
+        totalItems: total,
+        pageCount: Math.ceil(total / limit),
+        currentPage: page,
+      };
+
+      return { data: cursos, pagination };
+    } catch (error) {
+      this.logger.error(`Método: findAll, Error: ${error.message}`);
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        'Error interno al obtener los cursos.',
+      );
     }
-
-    const pagination = {
-      totalItems: total,
-      pageCount: Math.ceil(total / limit),
-      currentPage: page,
-    };
-
-    return { data: cursos, pagination };
   }
 
   async findOne(id: number): Promise<Curso> {
-    this.logger.log(`Servicio: CursosService, Método: findOne, Args: ${id}`);
+    this.logger.log(`Método: findOne, Args: ${id}`);
 
     try {
       const curso = await this.cursoRepository.findOne({ where: { id } });
       if (!curso) {
-        throw new NotFoundException({
-          message: `El curso con el ID ${id} no se encontró.`,
-          error: 'Not Found',
-          statusCode: 404,
-        });
+        throw new NotFoundException(`El curso con el ID ${id} no se encontró.`);
       }
       return curso;
     } catch (error) {
       this.logger.error(
-        `Error en Servicio: CursosService, Método: findOne, Args: ${id}, Error: ${error.message}`,
+        `Método: findOne, Args: ${id}, Error: ${error.message}`,
       );
-      throw new InternalServerErrorException({
-        message: error.message,
-        error: error.response?.error,
-        statusCode: error.status,
-      });
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        'Error interno al obtener el curso.',
+      );
     }
   }
 
@@ -128,10 +137,10 @@ export class CursosService {
     file: any,
   ): Promise<Curso> {
     this.logger.log(
-      `Servicio: CursosService, Método: update, Args: ${JSON.stringify({ id, updateCursoDto, file })}`,
+      `Método: update, Args: ${JSON.stringify({ id, updateCursoDto, file })}`,
     );
 
-    let updateCursoData = file
+    const updateCursoData = file
       ? { ...updateCursoDto, video: file.path }
       : updateCursoDto;
 
@@ -141,74 +150,68 @@ export class CursosService {
       if (file) {
         const video = curso.video;
         const filePath = path.join(__dirname, `../../${video}`);
-        async function deleteFile(filePath: string): Promise<void> {
-          try {
-            await fs.unlink(filePath);
-            console.log('Archivo eliminado exitosamente');
-          } catch (err) {
-            console.error('Error al eliminar el archivo:', err);
-          }
-        }
-        deleteFile(filePath);
+        await this.deleteFile(filePath);
       }
+
       const result = await this.cursoRepository.update(id, updateCursoData);
       if (result.affected === 0) {
-        throw new NotFoundException({
-          message: `El curso con el ID ${id} no se encontró.`,
-          error: 'Not Found',
-          statusCode: 404,
-        });
+        throw new NotFoundException(`El curso con el ID ${id} no se encontró.`);
       }
-      const cursoEditado = await this.findOne(id);
 
-      return cursoEditado;
+      return await this.findOne(id);
     } catch (error) {
       this.logger.error(
-        `Error en Servicio: CursosService, Método: update, Args: ${JSON.stringify({ id, updateCursoDto, file })}, Error: ${error.message}`,
+        `Método: update, Args: ${JSON.stringify({ id, updateCursoDto, file })}, Error: ${error.message}`,
       );
-      throw new InternalServerErrorException({
-        message: error.message,
-        error: error.response?.error,
-        statusCode: error.status,
-      });
+      if (
+        error instanceof QueryFailedError &&
+        error.message.includes('duplicate key value')
+      ) {
+        throw new ConflictException('Ya existe un curso con los mismos datos.');
+      }
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        'Error interno al actualizar el curso.',
+      );
     }
   }
 
   async remove(id: number): Promise<void> {
-    this.logger.log(`Servicio: CursosService, Método: remove, Args: ${id}`);
+    this.logger.log(`Método: remove, Args: ${id}`);
 
     try {
       const curso = await this.findOne(id);
-      const video = curso.video;
-      const filePath = path.join(__dirname, `../../${video}`);
-      async function deleteFile(filePath: string): Promise<void> {
-        try {
-          await fs.unlink(filePath);
-          console.log('Archivo eliminado exitosamente');
-        } catch (err) {
-          console.error('Error al eliminar el archivo:', err);
-        }
+      if (curso.video) {
+        const filePath = path.join(__dirname, `../../${curso.video}`);
+        await this.deleteFile(filePath);
       }
-
-      deleteFile(filePath);
 
       const result = await this.cursoRepository.delete(id);
       if (result.affected === 0) {
-        throw new NotFoundException({
-          message: `El curso con el ID ${id} no se encontró.`,
-          error: 'Not Found',
-          statusCode: 404,
-        });
+        throw new NotFoundException(`El curso con el ID ${id} no se encontró.`);
       }
     } catch (error) {
-      this.logger.error(
-        `Error en Servicio: CursosService, Método: remove, Args: ${id}, Error: ${error.message}`,
+      this.logger.error(`Método: remove, Args: ${id}, Error: ${error.message}`);
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        'Error interno al eliminar el curso.',
       );
-      throw new InternalServerErrorException({
-        message: error.message,
-        error: error.response?.error,
-        statusCode: error.status,
-      });
+    }
+  }
+
+  private async deleteFile(filePath: string): Promise<void> {
+    try {
+      await fs.unlink(filePath);
+      this.logger.log(`Archivo eliminado exitosamente: ${filePath}`);
+    } catch (err) {
+      this.logger.error(
+        `Error al eliminar el archivo: ${filePath}, Error: ${err.message}`,
+      );
+      throw new InternalServerErrorException('Error al eliminar el archivo.');
     }
   }
 }
